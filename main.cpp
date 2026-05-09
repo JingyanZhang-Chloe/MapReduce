b706855586a9ad4eb7d3c2e354344a64e1b01824
张晶妍 <zhangjingyan@zhangjingyandeMacBook-Air.local>
2026-05-09 12:35:46 +0200

    update

--- main.cpp
+++ main.cpp
@@ -1,11 +1,5 @@
 #include <iostream>
 
-template<class T, class U>
-T* map(U* array, size_t size_of_array, ) {
-    T* new_array = new T[size_of_array];
-}
-
-
 int main()
 {
     std::cout << "Hello, World!" << std::endl;
