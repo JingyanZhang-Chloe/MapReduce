fc05b4b19e62aaa71b814a7a0397d4c268fb3ff0
zhangjingyan <jingyan.zhang@polytechnique.edu>
2026-05-15 15:00:05 +0200

    adding vector insert opt for VisitedSet

--- include/VisitedSet.hpp
+++ include/VisitedSet.hpp
@@ -25,6 +25,47 @@ public:
         return inserted;
     }
 
+    bool insert_vector(const std::vector<U>& elements) {
+        // If insert fail, no element in the list will be inserted
+        std::lock_guard<std::mutex> guard(mutex);
+        std::unordered_set<U> tem;
+
+        for (const auto& element : elements) {
+            // check if it is in visited already
+            if (visited.find(element) != visited.end()) {
+                return false;
+            }
+
+            // check if elements has duplicates
+            auto [it, inserted] = tem.insert(element);
+            if (!inserted) {
+                return false;
+            }
+        }
+
+        std::unordered_set<U> inserted_set;
+
+        try {
+            for (const auto& element : elements) {
+                auto [it, inserted] = visited.insert(element);
+                if (!inserted) {
+                    // This should not happen in theory but just in case
+                    throw std::logic_error("insert_vector failed despite prior validation");
+                }
+
+                inserted_set.push_back(element);
+            }
+        } catch (...) {
+            for (const auto& element : inserted_set) {
+                visited.erase(element);
+            }
+
+            throw;
+        }
+
+        return true;
+    }
+
     bool contain(U element) {
         std::lock_guard<std::mutex> guard(mutex);
         return visited.find(element) != visited.end();
